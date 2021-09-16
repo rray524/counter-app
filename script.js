b389aacd2e11423e77494d77501303555f0f9bc1
rray524 <rray524@gmail.com>
2021-09-16 21:21:57 +0600

    add and substract function fixed

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 // find counter ID value
-let count = + document.querySelector('.count').innerText;
-console.log(count);
+let count = + document.getElementById('count').innerText;
+
+// add button event handler
 document.querySelector('.add').addEventListener('click', () => {
     count += 1;
+    let increasedText = document.getElementById('count');
+    increasedText.innerText = count;
+    if (count > 0) {
+        increasedText.style.color = 'yellow'
+    }
+    else {
+        increasedText.style.color = 'white'
+    }
+})
+
+// subtract button event handler
+document.querySelector('.subtract').addEventListener('click', () => {
+    count -= 1;
+    let decreasedText = document.getElementById('count');
+    decreasedText.innerText = count;
+    if (count < 0) {
+        decreasedText.style.color = 'red'
+    }
+    else {
+        decreasedText.style.color = 'white'
+    }
+})
+
+// Reset button handler
+document.querySelector('.reset').addEventListener('click', () => {
+    count -= 1;
+    let decreasedText = document.getElementById('count');
+    decreasedText.innerText = count;
+    if (count < 0) {
+        decreasedText.style.color = 'red'
+    }
 })
